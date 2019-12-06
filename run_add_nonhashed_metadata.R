bdd6b15d6e0c467ea1820e3cdd6ae5332028af8c
Lucas Graybuck <lucasg@alleninstitute.org>
2019-12-06 13:32:50 -0800

    changing halt condition

--- run_add_nonhashed_metadata.R
+++ run_add_nonhashed_metadata.R
@@ -32,7 +32,7 @@ opt_parser <- OptionParser(option_list = option_list)
 
 args <- parse_args(opt_parser)
 
-if(is.null(args$in_h5)) {
+if(is.null(args$out_html)) {
   print_help(opt_parser)
   stop("No parameters supplied.")
 }
